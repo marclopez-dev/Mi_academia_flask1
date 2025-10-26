8a221de76cfabd9421054b3ddd7585a9c2b48ce9
marclopez-dev <lopezpontemarc@gmail.com>
2025-10-26 01:43:50 -0500

    Preparando Flask para Render

--- web.py
+++ web.py
@@ -84,4 +84,5 @@ with app.app_context():
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
